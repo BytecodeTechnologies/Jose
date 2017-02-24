b75052d34540efeaa604cf233a18111604628f92
Vipan <vipin@bytecodetechnologies.in>
2017-02-24 14:58:04 +0530

    dui-newjersey.com

---
+++ Stabile DUI/Stabile DUI/Controllers/HomeController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewJersyTrafficTicket.Capcha;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+namespace Stabile_DUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("dwi-and-dui-results")]
+        public ActionResult dwi_and_dui_results()
+        {
+            return View("dwi-and-dui-results");
+        }
+        [ActionName("terms-and-definitions")]
+        public ActionResult terms_and_definitions()
+        {
+            return View("terms-and-definitions");
+        }
+        [ActionName("social-network")]
+        public ActionResult social_network()
+        {
+            return View("social-network");
+        }
+
+        [ActionName("contact-us")]
+        public ActionResult contact_us()
+        {
+            return View("contact-us");
+        }
+        //[ActionName("dui-resources")]
+        //public ActionResult dui_resources(string id)
+        //{
+        //    ViewBag.id = id;
+        //    return View("dui-resources");
+        //}
+        //public ActionResult generateCaptcha()
+        //{
+        //    System.Drawing.FontFamily family = new System.Drawing.FontFamily("Arial");
+        //    CaptchaImage img = new CaptchaImage(250, 100, family);
+        //    string text = img.CreateRandomText(4) + " " + img.CreateRandomText(3);
+        //    img.SetText(text);
+        //    img.GenerateImage();
+        //    img.Image.Save(Server.MapPath("~/CapchaImage/") +
+        //    this.Session.SessionID.ToString() + ".png",
+        //    System.Drawing.Imaging.ImageFormat.Png);
+        //    Session["captchaText"] = text;
+        //    return Json("../CapchaImage/" + this.Session.SessionID.ToString() + ".png?t=" +
+        //    DateTime.Now.Ticks, JsonRequestBehavior.AllowGet);
+        //}
+
+        public ActionResult Error()
+        {
+           return RedirectToAction("index");
+        }
+
+
+        public ActionResult Sendquery(string fullname = "", string CellNo = "", string Email = "", string Message = "")
+        {
+            var Name = fullname.Trim();
+            var captchatext = Convert.ToString(Session["captchaText"]);
+            var captchatexts = Regex.Replace(captchatext, @"\s+", "");
+            if (Email != "" && Name != "")
+            {
+                //if (captchatext == Capcha)
+                //{
+                    string to = "request@stabilelawfirm.com";
+                    string from = Email;
+                    string body = @"<html><title></title>
+              <head>
+                <style>
+body {
+            background: none;
+        }
+
+        table td {
+            height: 30%;
+            width: 50%;
+            border-color: #ff6801;
+        }
+
+        table th {
+            height: 70%;
+            width: 50%;
+            background-color: #ff6801;
+            color: white;
+            font-size: 100%;
+        }
+
+    </style>
+</head>
+<body>
+<div><table border=""1"" style='width:100%;'>
+<thead>
+<tr style='background:#ff6801;'>
+                        <th colspan=""2"" style='height: 70%;
+            width: 50%;
+            background-color: #ff6801;
+            color: white;
+            font-size: 100%;'>Query Form</th>
+
+                    </tr>
+           </thead>
+             <tbody>
+                      
+                    <tr>
+                        <td style='height: 100%; ;width: 50%; color:black; border-color: #ff6801;'>Full Name:</td>
+                        <td style='height: 100%;width: 50%; color:black; border-color: #ff6801;'>&nbsp;" + fullname + @"</td>
+                    </tr>
+
+
+                    <tr>
+                        <td style='height: 30%;width: 50%; color:black; border-color: #ff6801;'>Phone:</td>
+                        <td style='height: 30%;width: 50%; color:black; border-color: #ff6801;'>&nbsp;" + CellNo + @"</td>
+
+                    </tr>
+
+                      <tr>
+                        <td style='height: 30%;width: 50%; color:black; border-color: #ff6801;'>Email:</td>
+                        <td style='height: 30%;width: 50%; color:black; border-color: #ff6801;'>&nbsp;" + Email + @"</td>
+
+                    </tr>
+
+ <tr>
+                        <td style='height: 30%;width: 50%; color:black; border-color: #ff6801;'>Query:</td>
+                        <td style='height: 30%;width: 50%; color:black; border-color: #ff6801;'>&nbsp;" + Message + @"</td>
+
+                    </tr>
+   </tbody>
+</table >
+        </div>
+</body>
+<html>
+";
+                    MailMessage mail = new MailMessage(from, to);
+                    SmtpClient client = new SmtpClient();
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = false;
+                    client.Host = "mail.newjerseydefensivedriving.net";
+                    mail.Subject = "dui-newjersey.com has sent you another enquery Request!";
+                    mail.IsBodyHtml = true;
+                    mail.Body = body;
+                    try
+                    {
+                        client.Send(mail);
+                        return Json("Email Sent", JsonRequestBehavior.AllowGet);
+                    }
+                    catch (Exception ex)
+                    {
+                        return Json(ex.Message, JsonRequestBehavior.AllowGet);
+                    }
+                
+                //else
+                //{
+                //    return Json("Captcha text is not validated, please try again.", JsonRequestBehavior.AllowGet);
+                //}
+            }
+            else
+            {
+                return Json("* fields are required", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ Stabile DUI/Stabile DUI/Controllers/ResourcesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stabile_DUI.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        //
+        // GET: /Resources/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ActionName("DUI-in-School-Zone-or-School-Crossing")]
+        public ActionResult DUI_in_School_Zone_or_School_Crossing()
+        {
+            return View("DUI-in-School-Zone-or-School-Crossing");
+        }
+        [ActionName("Refusing-the-Chemical-Test")]
+        public ActionResult Refusing_the_Chemical_Test()
+        {
+            return View("Refusing-the-Chemical-Test");
+        }
+        [ActionName("Refusing-Chemical-Test-in-School-Zone-or-Crossing")]
+        public ActionResult Refusing_Chemical_Test_in_School_Zone_or_Crossing()
+        {
+            return View("Refusing-Chemical-Test-in-School-Zone-or-Crossing");
+        }
+        [ActionName("Boaters-convicted-of-driving-under-the-influence")]
+        public ActionResult Boaters_convicted_of_driving_under_the_influence()
+        {
+            return View("Boaters-convicted-of-driving-under-the-influence");
+        }
+        [ActionName("Driving-Under-the-Influence-of-Alcohol")]
+        public ActionResult Driving_Under_the_Influence_of_Alcohol()
+        {
+            return View("Driving-Under-the-Influence-of-Alcohol");
+        }
+
+        [ActionName("The-legal-age-in-NJ-to-purchase-an-alcoholic-beverage")]
+        public ActionResult The_legal_age_in_NJ_to_purchase_an_alcoholic_beverage()
+        {
+            return View("The-legal-age-in-NJ-to-purchase-an-alcoholic-beverage");
+        }
+        [ActionName("Interlock-Device-Requirements")]
+        public ActionResult Interlock_Device_Requirements()
+        {
+            return View("Interlock-Device-Requirements");
+        }
+        [ActionName("Surcharges-Alcohol-Violations")]
+        public ActionResult Surcharges_Alcohol_Violations()
+        {
+            return View("Surcharges-Alcohol-Violations");
+        }
+    }
+}
